a3872bd8ec90259c81df5cb5a8eaca02fa13d629
Isaac Cruz <isaac.clm1@gmail.com>
2021-07-29 17:55:51 -0300

    Updates

---
+++ precache-manifest.b61c12b68eb9ced33e208d893303a1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16db6e40b793aaeebf64a1d106d4935b",
+    "url": "/Movie-Card-Library/index.html"
+  },
+  {
+    "revision": "5c092f362fe035ab5877",
+    "url": "/Movie-Card-Library/static/css/main.d60e1a88.chunk.css"
+  },
+  {
+    "revision": "8de3077ad3618f3ac659",
+    "url": "/Movie-Card-Library/static/js/2.6705cb86.chunk.js"
+  },
+  {
+    "revision": "5c092f362fe035ab5877",
+    "url": "/Movie-Card-Library/static/js/main.6c39fa2e.chunk.js"
+  },
+  {
+    "revision": "13bb02e8cf4fd87d0819",
+    "url": "/Movie-Card-Library/static/js/runtime-main.145e7be2.js"
+  }
+]);
