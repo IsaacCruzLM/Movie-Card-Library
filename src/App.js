a29e44d759e4004e5300be86fd6f153e114bda90
Isaac Cruz <isaac.clm1@gmail.com>
2021-08-16 20:40:44 -0300

    Arruma Deploy

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import './App.css';
 function App() {
   const { MovieList, NewMovie, EditMovie, MovieDetails, NotFound } = routes;
   return (
-    <BrowserRouter basename="/Movie-Card-Library">
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
       <Header />
       <Switch>
-        <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route
           path="/movies/:id/edit"
           render={ (props) => <EditMovie { ...props } /> }
         />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route exact path="/" component={ MovieList } />
         <Route exact path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
